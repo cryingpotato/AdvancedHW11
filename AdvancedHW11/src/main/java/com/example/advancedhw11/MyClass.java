7fd32420784e591e11906aa8e7c3c42890732aac
cryingpotato <qq1591680@gmail.com>
2020-01-04 17:53:17 +0800

    Initial commit

---
+++ AdvancedHW11/src/main/java/com/example/advancedhw11/MyClass.java
@@ -0,0 +1,9 @@
+package com.example.advancedhw11;
+
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String[] args) {
+        Num prizeNumber = new Num("7_8.txt");
+        prizeNumber.Judge();
+    }
+}
